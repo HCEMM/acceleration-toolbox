6a897f56d52cfc1272c806af4eb1f84544fcab15
MOHAMED COULIBALY <79422585+MOHAMED2000-hub@users.noreply.github.com>
2026-07-06 16:47:31 +0200

    Implement FastQC command in qc.sh
    Added FastQC command to process gzipped reads and output results.

--- 01_GitHub/scripts/01-qc.sh
+++ 01_GitHub/scripts/01-qc.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Activate conda environment
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate workshop
+
+
 READS_DIR=$1
 RESULTS_DIR=$2/qc
 THREADS=$3
@@ -14,7 +19,7 @@ echo "Running FastQC on raw reads"
 # READS ARE GZIPPED AT $READS_DIR
 # OUTPUT SHOULD GO TO $RESULTS_DIR
 # USE $THREADS
-
+fastqc "$READS_DIR"/*.gz --outdir "$RESULTS_DIR" --threads "$THREADS"
 
 # Check if 12 FastQC reports were generated, and exit code 1 if not
 if [ $(ls "$RESULTS_DIR"/*_fastqc.zip | wc -l) -ne $N_FILES ]; then
