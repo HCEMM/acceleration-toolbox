0f08508d994a6ce70b43589b6e460be7d7c01544
benoalexandra <beno.alexandra6@gmail.com>
2026-07-06 16:45:33 +0200

    group3

--- 01_GitHub/scripts/03-annotation.sh
+++ 01_GitHub/scripts/03-annotation.sh
@@ -20,6 +20,7 @@ do
 done
 
 
+
 # check if 12 annotation results were generated, and exit code 1 if not
 if [ $(ls "$RESULTS_DIR"/*_matches.tab | wc -l) -ne $N_FILES ]; then
     echo "Error: Not all annotation results were generated. Expected $N_FILES, but found $(ls "$RESULTS_DIR"/*_matches.tab | wc -l)."
